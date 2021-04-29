3eb1d181735fe0f91c8a36ef52bcb83af66e0596
Rod-S <rodsanati@gmail.com>
2021-04-28 23:31:07 -0500

    add spring boot initializr files

---
+++ src/test/java/com/galleryapp/galleryapp/GalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galleryapp.galleryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
