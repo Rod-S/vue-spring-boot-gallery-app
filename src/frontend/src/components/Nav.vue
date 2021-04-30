7d522ba08cebde3772be071bf6975cc219bfffe3
Rod-S <rodsanati@gmail.com>
2021-04-29 23:36:17 -0500

    add base templates and css files

---
+++ src/frontend/src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+  <nav className="main-nav">
+  <ul>
+    <li><NavLink to='/search/sunglasses'>Sunglasses</NavLink></li>
+    <li><NavLink to='/search/hats'>Hats</NavLink></li>
+    <li><NavLink to='/search/watches'>Watches</NavLink></li>
+  </ul>
+</nav>
+<script>
+export default {
+  name: "Nav.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
